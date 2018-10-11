b63398d52471d5cdbef9b3737851a6a292dc7028
thangbx <thangbx@coccoc.c>
2018-10-11 15:01:01 +0700

    Add loss function

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -15,6 +15,11 @@ x = tf.placeholder(tf.float32)
 # Create model
 linear_model = W*x + b
 
+# Loss function
+y = tf.placeholder(tf.float32)
+loss = tf.square(linear_model - y)
+total_loss = tf.reduce_sum(loss)
+
 # Init variables
 sess.run(tf.global_variables_initializer())
 
@@ -23,4 +28,12 @@ new_w = tf.assign(W, -1)
 new_b = tf.assign(b, 1)
 sess.run([new_w, new_b])
 
-print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+# Training data
+source = [1, 2, 3, 4]
+dist = [0, -1, -2, -3]
+
+# Run model
+print("Exec: ", str(sess.run(linear_model, {x: source})))
+
+# Calc loss
+print("Loss: ", float(sess.run(total_loss, {x: source, y: dist})))
