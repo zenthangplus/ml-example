3bb2d519ccfe5ee37ca736fac4f94d1d2eeea722
thangbx <thangbx@coccoc.c>
2018-10-11 15:43:48 +0700

    Optimize and calculate W, b from training data

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -4,9 +4,6 @@ import tensorflow as tf
 # Just disables the warning, doesn't enable AVX/FMA
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Init session
-sess = tf.Session()
-
 # Declare variables
 W = tf.Variable(0, dtype=tf.float32)
 b = tf.Variable(0, dtype=tf.float32)
@@ -20,20 +17,21 @@ y = tf.placeholder(tf.float32)
 loss = tf.square(linear_model - y)
 total_loss = tf.reduce_sum(loss)
 
-# Init variables
-sess.run(tf.global_variables_initializer())
-
-# Assign new value
-new_w = tf.assign(W, -1)
-new_b = tf.assign(b, 1)
-sess.run([new_w, new_b])
+# Optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(total_loss)
 
 # Training data
-source = [1, 2, 3, 4]
-dist = [0, -1, -2, -3]
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
 
-# Run model
-print("Exec: ", str(sess.run(linear_model, {x: source})))
+# Init variables
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-# Calc loss
-print("Loss: ", float(sess.run(total_loss, {x: source, y: dist})))
+# Run training
+for i in range(1000):
+    sess.run(train, {x: x_train, y: y_train})
+
+curr_W, curr_b, curr_model, curr_loss = sess.run([W, b, linear_model, total_loss], {x: x_train, y: y_train})
+print("W: %s b: %s model: %s, loss: %s" % (curr_W, curr_b, curr_model, curr_loss))
